d583317b3cd98a8983c1001f710a771b8cc1c513
TheDreamSkater <thedreamskater@gmail.com>
2023-05-21 22:03:05 -0400

    Updates: 1) POM.xml==>> TestNG Version updated to 7.4.0 from 7.7.1 2) Adding ThirdTest.java 3) Removing thread-counts from testng.xml

---
+++ src/test/java/baseClasses/ThirdTest.java
@@ -0,0 +1,12 @@
+package baseClasses;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest {
+	
+	@Test
+	public void third() throws InterruptedException
+	{
+		System.out.println("Third Test");
+	}
+}
